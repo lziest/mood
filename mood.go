36befa2556c8d88c9f5add32eb944b07d6171409
Zi Lin <zi@cloudflare.com>
2016-06-12 23:42:14 -0700

    re-document

--- mood.go
+++ mood.go
@@ -1,3 +1,5 @@
+// package mood hsows a example of a exported method whose behavior
+// is controlled by package exported variable.
 package mood
 
 import (
@@ -5,12 +7,11 @@ import (
 	"fmt"
 )
 
-var Status = "sad"
+// Status is a simple string
+var Status = "happy"
 
-func init() {
-	// Only define flag once.
-	if flag.Lookup("mood") == nil {
-		flag.StringVar(&Status, "mood", "sad", "mood is a string")
-	}
+// Show shows mood status
+func Show() {
 	fmt.Printf("mood.Status ptr = %p\n", &Status)
+	fmt.Println("I'm", Status)
 }
